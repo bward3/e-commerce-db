adaaa75c5bb66cc45c80311123f2fb4fd3e5fd86
Brian Ward <brianwardcs@gmail.com>
2022-05-20 17:43:49 -0600

    sequelize db models added

---
+++ models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  unique: false
+});
+
+Tag.belongsToMany(Product, {
+  through: Tag,
+  unique: false,
+});
+
+// Tags belongToMany Products (through ProductTag)
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
